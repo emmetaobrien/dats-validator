d84c2b9834e7b22ea0c1b97bb46f049102a179a5
gi114 <giulia.ippoliti@mail.mcgill.ca>
2019-07-26 10:47:39 -0400

    Created functions for checking required and recommended fields

--- dats/content-checker.py
+++ dats/content-checker.py
@@ -6,13 +6,34 @@
 #  - Notifies with Warning if RECOMMENDED keys are missing and what they are, returns True
 #  - else returns True
 
-
+import logging
 import json
 import os.path as op
 
-required = []
-recommended = []
+required = ['$schema', '@type', 'title', 'types', 'creators', 'keywords', 'version']
+recommended = ['identifier', 'dates', 'dimensions', 'primaryPublications']
 optional = []
+
+# We have a list of required and a list of recommended. For each of the elements we check if they are contained in data
+# We do not need to search through nested data structures as the terms we are checking for are 1st level
+# e.g. data['@type']
+
+
+def check_required(json_data):
+    flag = True
+    for value in required:
+        if not json_data[value]:
+            logging.error("The required key " + value + "is missing")
+            flag = False
+    return flag
+
+
+def check_recommended(json_data):
+    for value in recommended:
+        if not json_data[value]:
+            logging.warn("The recommended key " + value + "is missing")
+    return True
+
 
 json_file = 'dataset.json'
 
@@ -23,8 +44,12 @@ with open(json_path) as file:
     data = json.load(file)
 
 print(data)
+if check_required(data) and check_recommended(data):
+    logging.info("Validation went OK")
 
-# We have a list of required and a list of recommended. For each of the elements we check if they are contained in data
-# We do not need to search through nested data structures as the terms we are checking for are 1st level
-# e.g. data['@type']
+
+
+
+
+
 
